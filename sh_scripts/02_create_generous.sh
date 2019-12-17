f48ceddd9908e241d9c861a2287b23410a061a2b
kstasi <nastasiakondaurova@gmail.com>
2019-12-17 10:50:59 +0200

    generous scripts updated

--- sh_scripts/02_create_generous.sh
+++ sh_scripts/02_create_generous.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 func -P -o build/02-generous-code.fif contracts/stdlib.fc contracts/02-generous-code.fc
 fift -s fift_scripts/02-new-generous.fif
+fift -s fift_scripts/02-attack-generous.fif
